6970b0d30a43f6b6d0b2ad60456577a4746f64e7
David Ribeiro <davidflribeiro87@gmail.com>
2019-12-06 23:20:23 -0100

    finished

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Header from './Header/Header';
+import Main from './Main/Main';
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+
+import './App.css';
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+class App extends Component {
+  state = {
+    selected: {
+      Processor: {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
+      },
+      'Operating System': {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      'Video Card': {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
+    }
+  };
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  render() {
+    const total = Object.keys(this.state.selected).reduce(
+      (acc, curr) => acc + this.state.selected[curr].cost,
+      0
+    );
+
+    return (
+      <div className='App'>
+        <Header />
+        <Main
+          features={this.props.features}
+          selected={this.state.selected}
+          updateFeature={this.updateFeature}
+          total={total}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
